8436ab3dbd24b2553b877c3a7fa8a0f2661b5f35
Ebubejisos <anosikegideon007@gmail.com>
2023-08-20 23:26:27 +0100

    updated

--- pages/api/flutter/like/index.js
+++ pages/api/flutter/like/index.js
@@ -1,25 +1,41 @@
-import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
 
 export default withApiAuthRequired(async function handler(req, res) {
   const { accessToken } = await getAccessToken(req, res);
   const fetchOptions = {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Request-Headers": "*",
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
       jwtTokenString: accessToken,
     },
   };
   const fetchBody = {
     dataSource: process.env.MONGODB_DATA_SOURCE,
-    database: "social_butterfly",
-    collection: "flutters",
+    database: 'social_butterfly',
+    collection: 'flutters',
   };
   const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
 
   try {
     switch (req.method) {
-      // Add Update Functionality Here
+      case 'PUT':
+        const updateData = await fetch(`${baseUrl}/updateOne`, {
+          ...fetchOptions,
+          body: JSON.stringify({
+            ...fetchBody,
+            filter: { _id: { $oid: req.body._id } },
+            update: {
+              [req.body.action]: {
+                likes: req.body.userId,
+              },
+            },
+          }),
+        });
+        const updateDataJson = await updateData.json();
+        res.status(200).json(updateDataJson);
+        break;
+
       default:
         res.status(405).end();
         break;
